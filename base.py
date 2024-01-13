fec8903eaf8461518b1dac5547b804937a8a56d9
Parth Asawa <pgasawa@berkeley.edu>
2024-01-13 13:09:20 -0500

    refactor to scale running apple scripts

--- base.py
+++ base.py
@@ -1,18 +1,17 @@
 import subprocess
 import functions
 
-def open_application_with_parameter(application_name):
-    script = functions.openApplication
-    
-    # Save the AppleScript to a temporary file
-    script_path = "openApplication.applescript"
+def runAppleScript(script: str, parameters: list):    
+    # Write the AppleScript to a temporary file
+    script_path = "temp.applescript"
     with open(script_path, "w") as script_file:
         script_file.write(script)
 
     try:
         # Run the AppleScript using osascript
-        subprocess.run(["osascript", script_path, application_name], check=True)
+        subprocess.run(["osascript", script_path, *parameters], check=True)
     finally:
         # Clean up the temporary script file
         subprocess.run(["rm", script_path])
-open_application_with_parameter("Safari")
+
+runAppleScript(functions.closeApplication, ["Safari"])
--- functions.py
+++ functions.py
@@ -6,3 +6,12 @@ openApplication = """
         end tell
     end run
     """
+
+closeApplication = """
+    on run argv
+        set appName to item 1 of argv
+        tell application appName
+            quit
+        end tell
+    end run
+    """
